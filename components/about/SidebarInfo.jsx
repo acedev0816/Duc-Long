1c41dd6441bbb383e26bbaf16fee2f3a1ae5c234
acedev0816 <acedev0816@gmail.com>
2024-12-16 23:10:01 +0800

    fix

--- components/about/SidebarInfo.jsx
+++ components/about/SidebarInfo.jsx
@@ -31,7 +31,7 @@ const SidebarInfo = () => {
         </div>
 
         <a
-          href="/Chris Ma - resume.docx"
+          href="/Chu Duc Long.pdf"
           download
           className="inline-flex items-center mx-auto bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476]  to-[#fa5252ef] px-8 py-3 text-lg text-white rounded-[35px] mt-6"
         >
